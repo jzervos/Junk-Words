5f90c6f2d1b834f8764bc193ebd73be192d14105
John <jzervos@emich.edu>
2018-03-10 22:18:50 +0000

    Create main.java

---
+++ main.java
@@ -0,0 +1,31 @@
+/*
+ * Main Java file
+ * At this time just throwing down a basic skeleton
+ */
+ 
+ import java.io.*;
+ import java.util.*;
+ import java.lang.*;
+ public class Junk
+ {
+  public static void main (String [] args)
+  {
+    // set up alphabet array
+    char alphabet[q,w,e,r,t,y,u,i,o,p,l,k,j,h,g,f,d,s,a,z,x,c,v,b,n,m];
+    int letter; // an int containing the position of the letter to be used
+  
+    // get input from the user
+    int numOfWords;
+    int lengthOfWord;
+    String fileName;
+    Scanner keyboard = new Scanner(System.in);
+    Scanner fileOut = null;
+    
+    // ask number of words and file name
+
+    while (numOfWords)  // keep looping until printed every word
+    {
+      // random loop for each word
+    
+    }
+
